951c4041f76aa183665b70a25b57f43d34f0c6c0
meenakshisundaram-seeni <meenakshisundaram.seeni@gmail.com>
2025-11-01 20:14:33 +0530

    created arithmetic python file

---
+++ basicArithmetic.py
@@ -0,0 +1,13 @@
+num01 = int ( input ('Enter the first number '))
+num02 = int ( input ('Enter the second number '))
+
+addition = num01 + num02
+subtraction = num01 - num02
+multiplication = num01 * num02
+division = num01 / num02
+
+print ('Addition:',addition)
+print ('Subtraction:',subtraction)
+print ('Multiplication:',multiplication)
+print ('Division:',division)
+
